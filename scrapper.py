848269d25248c256b5c116b581a03220ad0e7cca
Yves-Ganza <lebonganza@outlook.com>
2022-01-03 01:30:13 -0500

    Implemented Automated 2-Factor Authentication

--- scrapper.py
+++ scrapper.py
@@ -13,9 +13,11 @@ load_dotenv()
 PATH = '/Users/mack/geckodriver'
 driver = webdriver.Firefox(executable_path=PATH)
 
+
 def get_security_code():
     r = requests.post(os.getenv('SERVER_URL'), data={'body': 'request:security-code'})
     return r
+
 
 print('Loading page...')
 driver.get('https://outlook.com')
@@ -30,7 +32,10 @@ try:
     email_input = driver.find_element(By.NAME, 'loginfmt')
     email_input.send_keys(os.getenv('EMAIL'), Keys.RETURN)
     print('Done!')
+except Exception as e:
+    print('Email input skipped!')
 
+try:
     WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.NAME, 'passwd')))
 
     print('Waiting for password input...')
@@ -38,12 +43,21 @@ try:
     pwd_input.send_keys(os.getenv('PASSWORD'))
     pwd_input.send_keys(Keys.RETURN)
     print('Done!')
+except Exception as e:
+    print('Password input skipped!')
 
+try:
     WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.NAME, 'iProofPhone')))
 
     print('2FA initiated...')
     phone_proof = driver.find_element(By.NAME, 'iProofPhone')
-    phone_proof.send_keys(os.getenv('DIGITS'), Keys.RETURN)
+
+    if phone_proof:
+        phone_proof.send_keys(os.getenv('DIGITS'), Keys.RETURN)
+except Exception as e:
+    print('2-Factor Authentication skipped!')
+
+try:
     WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.NAME, 'iOttText')))
 
     proof_text = driver.find_element(By.NAME, 'iOttText')
@@ -54,7 +68,31 @@ try:
     print(r.text)
     print('Done!')
 except Exception as e:
-    print(e)
+    print('Security code skipped!')
+
+try:
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.NAME, 'iOttText')))
+
+    code = driver.find_element(By.NAME, 'iOttText')
+
+    if code:
+        print('Entering security code...')
+        code.send_keys(r.text)
+        code.send_keys(Keys.RETURN)
+        print('Done!')
+except Exception as e:
+    print('Entering security code skipped!')
+
+try:
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, 'idSIButton9')))
+
+    yes_btn = driver.find_element(By.ID, 'idSIButton9')
+
+    if yes_btn:
+        yes_btn.click()
+        print('Yes button clicked!')
+except Exception as e:
+    print('Button click skipped!')
 
 
 
